e718c0e74484326e70c8a1d129b48ae11ec4782f
Kenny Nguyen <sokenns@gmail.com>
2024-02-24 14:49:21 -0800

    Add placeholder text

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+
 function Home() {
-  return <main>Home</main>;
+  return (
+    <main>
+      <h1>Home</h1>
+      <p>Welcome to the Store.</p>
+      <Link to="/shop">Shop all our products!</Link>
+    </main>
+  );
 }
 
 export default Home;
