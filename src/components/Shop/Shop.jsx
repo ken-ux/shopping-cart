011c32fc392a54f6e13151f81781b40c043b2ab9
Kenny Nguyen <sokenns@gmail.com>
2024-02-24 14:49:07 -0800

    Fetch data in ShopItem and show on Shop page

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,5 +1,13 @@
+import ShopItem from "../ShopItem/ShopItem.jsx";
+
 function Shop() {
-  return <main>Shop</main>;
+  return (
+    <main>
+      <h1>Store</h1>
+      <p>Browse our wide selection of products:</p>
+      <ShopItem itemId={1} />
+    </main>
+  );
 }
 
 export default Shop;
---
+++ src/components/ShopItem/ShopItem.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import PropTypes from "prop-types";
+import styles from "./ShopItem.module.css";
+
+function ShopItem({ itemId }) {
+  const [itemData, setItemData] = useState({});
+  const [count, setCount] = useState(0);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function getImageUrl() {
+      try {
+        const response = await fetch(
+          "https://fakestoreapi.com/products/" + itemId
+        );
+        const data = await response.json();
+        const relevantData = {
+          description: data.description,
+          image: data.image,
+          price: data.price,
+          title: data.title,
+        };
+        setItemData(relevantData);
+      } catch (error) {
+        console.error("Issue fetching data: " + error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    getImageUrl();
+  }, [itemId]);
+
+  if (loading) {
+    return <h2>Loading product...</h2>;
+  }
+
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>{itemData.title}</h2>
+      <img className={styles.image} src={itemData.image} alt="" />
+      <p>{itemData.description}</p>
+      <p>${itemData.price}</p>
+      <div className={styles.itemCounter}>
+        <button
+          type="button"
+          onClick={() => {
+            if (count > 0) {
+              setCount(count - 1);
+            }
+          }}
+        >
+          -
+        </button>
+        <p>{count}</p>
+        <button type="button" onClick={() => setCount(count + 1)}>
+          +
+        </button>
+      </div>
+
+      <button type="button">Add to Cart</button>
+    </div>
+  );
+}
+
+ShopItem.propTypes = {
+  itemId: PropTypes.number,
+};
+
+export default ShopItem;
