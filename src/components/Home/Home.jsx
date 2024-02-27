75b0b74704f61dd0c7d4cf04c78adf51a6b2cf4e
Kenny Nguyen <sokenns@gmail.com>
2024-02-27 00:08:05 -0800

    Add styles and image to home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,18 @@
 import { Link } from "react-router-dom";
+import computerImage from "../../assets/computer_parts.jpeg";
+import styles from "./Home.module.css";
 
 function Home() {
   return (
     <main>
       <h1>Home</h1>
-      <p>Welcome to the Store.</p>
-      <Link to="/shop">Shop all our products!</Link>
+      <div className={styles.container}>
+        <div className={styles.heroText}>
+          <p>Looking for PC parts and accessories?</p>
+          <Link to="/shop">Shop all our products!</Link>
+        </div>
+        <img className={styles.image} src={computerImage} alt="" />
+      </div>
     </main>
   );
 }
